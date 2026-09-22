217220590f5eb3805ff79aae49f7dda1a69db5ff
Khoi C <khoi.cao1@education.nsw.gov.au>
2026-09-22 22:34:30 +1000

    feature: anchors minigame

---
+++ Assets/Scripts/AnchorFunction.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
+
+public class AnchorFunction : MonoBehaviour
+{
+    public GameObject wheel;
+    public GameObject spinner;
+    public Image fill;
+    public Sprite[] stages;
+    public Tilemap targetTilemap;
+    public TileBase greenAnchorTile;
+
+    private float targetPercent;
+    private float percent;
+    private float wheelRotation;
+    private float spinnerRotation;
+    private int spinDir;
+    private Image wheelImg;
+    private GameObject currentAnchor;
+
+    AudioManager audioManager;
+
+    private void Awake() {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
+    void Start() {
+        wheelImg = wheel.GetComponent<Image>();
+        targetPercent = 0f;
+        percent = 0f;
+        spinDir = 1;
+        gameObject.SetActive(false);
+    }
+
+    public void Reset(GameObject anchor) {
+        targetPercent = 0f;
+        percent = 0f;
+        spinDir = 1;
+        gameObject.SetActive(true);
+        currentAnchor = anchor;
+    }
+
+    void Update()
+    {
+        spinner.transform.Rotate(new Vector3(0, 0, 180 * spinDir) * Time.deltaTime);
+        percent += (targetPercent - percent) / 10f;
+        fill.fillAmount = percent;
+        if (targetPercent >= 0.7f) {
+            wheelImg.sprite = stages[2]; 
+        } else if (targetPercent >= 0.4f) {
+            wheelImg.sprite = stages[1]; 
+        } else {
+            wheelImg.sprite = stages[0];
+        }
+    }
+
+    public void Lock() {
+        if (targetPercent < 1) {
+            wheelRotation = wheel.transform.eulerAngles.z;
+            spinnerRotation = spinner.transform.eulerAngles.z;
+            float angleDifference = Mathf.Abs(Mathf.DeltaAngle(wheelRotation, spinnerRotation));
+            if (angleDifference < 50 && targetPercent < 0.4f || angleDifference < 35 && targetPercent < 0.7f || angleDifference < 20 && targetPercent >= 0.7f) {
+                targetPercent += 0.1f;
+                audioManager.PlaySFX(audioManager.hover);
+            } else if (targetPercent > 0f) {
+                targetPercent -= 0.1f;
+                audioManager.PlaySFX(audioManager.click);
+            }
+            wheel.transform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
+            spinDir = spinDir * -1;
+        }
+        if (percent >= 0.97) {
+            gameObject.SetActive(false);
+            Vector3Int cellPosition = targetTilemap.WorldToCell(currentAnchor.transform.position);
+            targetTilemap.SetTile(cellPosition, greenAnchorTile);
+            Destroy(currentAnchor);
+        }
+    }
+}
---
+++ Assets/Scripts/AnchorInteract.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class AnchorInteract : MonoBehaviour
+{
+    public InteractButton interactRef;
+    public AnchorFunction anchorRef;
+
+    private bool isPlayerTouching;
+
+    void Update() {
+        if (isPlayerTouching)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anchorRef.Reset(gameObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerTouching = true;
+            interactRef.ActiveState(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactRef.ActiveState(false);
+        }
+    }
+}
